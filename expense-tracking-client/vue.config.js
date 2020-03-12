7c5e0b5627bb8f71181cfbeb7ea6a3369b9d6430
Hoang N. Tran <hoangtn1304@gmail.com>
2020-03-12 20:43:03 +0700

    make client work

--- expense-tracking-client/vue.config.js
+++ expense-tracking-client/vue.config.js
@@ -4,5 +4,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  productionSourceMap: true
+  productionSourceMap: false,
+  // outputDir: path.resolve(__dirname, "../vue-expenses-api/wwwroot"),
+  // assetsDir: "../../vue-expenses-api/wwwroot/static"
 }
