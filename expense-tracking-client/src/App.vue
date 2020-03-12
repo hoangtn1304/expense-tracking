e6d7409f691792d6e716309e1475ab182f53cde7
Hoang N. Tran <hoangtn1304@gmail.com>
2020-03-12 17:54:51 +0700

    add client app

--- expense-tracking-client/src/App.vue
+++ expense-tracking-client/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-snackbar v-model="alert.show" :color="alert.color" top>
+      {{alert.message}}
+      <v-icon small dark @click="alert.show = false">close</v-icon>
+    </v-snackbar>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  computed: {
+    ...mapState({
+      alert: state => state.alert.alert
+    })
+  },
+  data() {
+    return {};
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ expense-tracking-client/src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import HomePage from '@/views/HomePage.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import Expenses from '@/views/Expenses.vue'
+import Stats from '@/views/Stats.vue'
+import Settings from '@/views/Settings.vue'
+import Profile from '@/views/Profile.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/', component: HomePage,
+      children: [
+        //HomePage's <router-view>
+        { path: '/dashboard', component: Dashboard },
+        { path: '/expenses', component: Expenses },
+        { path: '/stats', component: Stats },
+        { path: '/settings', component: Settings },
+        { path: '/profile', component: Profile },
+        { path: '/', component: Dashboard },
+      ]
+    },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    // otherwise redirect to home
+    { path: '*', redirect: '/' }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+})
+
+export default router;
